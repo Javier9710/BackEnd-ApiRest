a1bdd041a093ac4a045a363c504adb6e2293bf03
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-14 18:52:10 -0500

    Creando controlador restController para listar clientes

---
+++ src/main/java/com/ufps/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,27 @@
+package com.ufps.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufps.springboot.backend.apirest.models.entities.Cliente;
+import com.ufps.springboot.backend.apirest.models.service.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		
+		return clienteService.findAll();
+		
+	}
+
+}
