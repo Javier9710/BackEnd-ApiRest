202f16579e5f504fd6c583714b150a038c4a942c
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-14 18:54:19 -0500

    Importando datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+//Clientes
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Javier', 'Moncada', 'javier@ufps.edu.co','2021-01-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Camilo', 'Delgado', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andres', 'Boada', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Carlos', 'Diaz', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gabriela', 'Suarez', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jean', 'Moncada', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mafer', 'Mantilla', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Brayan', 'Jose', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cesar', 'Patiño', 'Camilo@ufps.edu.co','2021-01-12');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Stewart', 'Rojas', 'Camilo@ufps.edu.co','2021-01-12');
